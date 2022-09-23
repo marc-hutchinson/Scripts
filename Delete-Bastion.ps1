f4c5c4727ce1ac9b0b9a9c89b214469a11820571
marc-hutchinson <69528649+marc-hutchinson@users.noreply.github.com>
2022-09-23 14:25:08 +0100

    Update Delete-Bastion.ps1

--- Delete-Bastion.ps1
+++ Delete-Bastion.ps1
@@ -3,6 +3,6 @@ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Content-Type", "application/json")
 $body = "{`"Action`":`"Delete`"}"
 
-$RequestURL = "https://prod-20.uksouth.logic.azure.com:443/workflows/5ff69f8161ce4a48a6d3b205620b8e00/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=_EvPBi1iVTepvXIF_PxSaskTdxfrcqPj2nPGA2M-FpY"
+$RequestURL = "https://prod-29.uksouth.logic.azure.com:443/workflows/01c9ba1db3f348558b71fc26bffc55b6/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=LpWebC_oQCEa1yPttjsTHaqjnf3o-i5XnOqdFMisq6g"
 
 $response = Invoke-RestMethod $RequestURL -Method 'POST' -Headers $headers -Body $body
