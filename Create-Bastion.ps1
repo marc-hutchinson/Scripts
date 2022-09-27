0e67f747a35633bff3dd7e3fd162113c970344c2
marc-hutchinson <69528649+marc-hutchinson@users.noreply.github.com>
2022-09-27 12:20:32 +0100

    Update Create-Bastion.ps1

--- Create-Bastion.ps1
+++ Create-Bastion.ps1
@@ -3,6 +3,6 @@ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Content-Type", "application/json")
 $body = "{`"Action`":`"Deploy`"}"
 
-$RequestURL = "https://prod-29.uksouth.logic.azure.com:443/workflows/01c9ba1db3f348558b71fc26bffc55b6/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=LpWebC_oQCEa1yPttjsTHaqjnf3o-i5XnOqdFMisq6g"
+$RequestURL = "https://prod-13.uksouth.logic.azure.com:443/workflows/0ada8d41931a4b8db7c2c3dd32f39299/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=hX8fgHdm4WFcM0Xke4MdqlSHW6Va5sMaNNvAJnj0i3k"
 
 $response = Invoke-RestMethod $RequestURL -Method 'POST' -Headers $headers -Body $body
